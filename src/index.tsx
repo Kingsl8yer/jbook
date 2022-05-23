c21bbab7f54026bdfa75740f1c061c64515e91d9
Miguel Varona <migueluk91@gmail.com>
2022-05-23 14:38:13 +0100

    Icon Buttons

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import {Provider} from "react-redux";
 import {store} from "./state";
 import CellList from "./components/cell-list";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const App = () => {
     return (
